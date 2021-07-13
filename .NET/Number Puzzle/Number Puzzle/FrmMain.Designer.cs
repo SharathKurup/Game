21f81028430c38151d4b4f91061bbdf114eb6e89
Sharath Kurup <hari.19113@gmail.com>
2021-07-13 09:46:46 +0530

    Initial Commit. Shuffle Number controls randomly.

---
+++ .NET/Number Puzzle/Number Puzzle/FrmMain.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Number_Puzzle
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.tblControls = new System.Windows.Forms.TableLayoutPanel();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlMain.Controls.Add(this.tblControls);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(482, 453);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // tblControls
+            // 
+            this.tblControls.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tblControls.ColumnCount = 4;
+            this.tblControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tblControls.Location = new System.Drawing.Point(0, 0);
+            this.tblControls.Name = "tblControls";
+            this.tblControls.RowCount = 4;
+            this.tblControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tblControls.Size = new System.Drawing.Size(482, 453);
+            this.tblControls.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.pnlMain);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Puzzle";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.TableLayoutPanel tblControls;
+    }
+}
+
---
+++ .NET/Number Puzzle/Number Puzzle/FrmMain.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Number_Puzzle
+{
+    public partial class FrmMain : Form
+    {
+        private Button btnNumber;
+        private readonly List<int> randomList = new List<int>();
+        private readonly Random random = new Random();
+        public FrmMain()
+        {
+            InitializeComponent();
+            InitForm();
+        }
+
+        private void InitForm()
+        {
+            AddCloseButton();
+            int iCount = 1;
+
+            for (int iCol = 0; iCol < 4; iCol++)
+            {
+                for (int iRow = 0; iRow < 4; iRow++)
+                {
+                    btnNumber = new Button()
+                    {
+                        Name = $"btn{iCount}",
+                        Text = $"{iCount}",
+                        Dock = DockStyle.Fill,
+                    };
+                    if (btnNumber.Name != "btn16")
+                        tblControls.Controls.Add(btnNumber, iRow, iCol);
+
+                    iCount++;
+                }
+            }
+        }
+
+        private void ShuffleNumbers()
+        {
+            int iRand = GetRandomNumber();
+            Button tmpButtonControl = (Button)tblControls.Controls[iRand];
+            int iCol = tblControls.GetColumn(tmpButtonControl);
+            int iRow = tblControls.GetRow(tmpButtonControl);
+            tblControls.Controls.Add(tmpButtonControl, 3, 3);
+
+            for (int iCount = 0; iCount < 12; iCount++)
+            {
+                iRand = GetRandomNumber();
+                tmpButtonControl = (Button)tblControls.Controls[iRand];
+                int iCol1 = tblControls.GetColumn(tmpButtonControl);
+                int iRow1 = tblControls.GetRow(tmpButtonControl);
+                tblControls.Controls.Remove(tmpButtonControl);
+                tblControls.Controls.Add(tmpButtonControl, iCol, iRow);
+                iCol = iCol1;
+                iRow = iRow1;
+            }
+        }
+
+        private int GetRandomNumber()
+        {
+            int iRandom = random.Next(0, 15);
+            while (randomList.Contains(iRandom))
+            {
+                iRandom = random.Next(0, 15);
+            }
+            randomList.Add(iRandom);
+            return iRandom;
+        }
+
+        private void AddCloseButton()
+        {
+            Button btnClose = new Button();
+            btnClose.Click += (o, i) => Close();
+            Controls.Add(btnClose);
+            CancelButton = btnClose;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            ShuffleNumbers();
+        }
+    }
+}
